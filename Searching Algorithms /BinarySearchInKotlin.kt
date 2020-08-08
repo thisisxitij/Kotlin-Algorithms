783b8243dbbea1f34e0ef5b4e35c4068b3880a9b
thisisxitij <xitij10@gmail.com>
2020-08-08 23:38:03 +0530

    added some comments to explain things out.

--- Searching Algorithms /BinarySearchInKotlin.kt
+++ Searching Algorithms /BinarySearchInKotlin.kt
@@ -20,10 +20,12 @@ fun search(value: Int, array: Array<Int>): Boolean {
     var lowerBond = 0
     var upperBond  = array.size - 1
 
-    while (lowerBond <= upperBond) {
-        val midPoint = lowerBond + upperBond  / 2
+    while (lowerBond <= upperBond) { // lower bond must be smaller or equal to upperBond so that they can't pass each other
+        val midPoint = lowerBond + upperBond  / 2 // getting middle index of array where is lower bond is first index and upper Bond is last index Of array
 
         if(array[midPoint] == value) return true // checking the value on mid point
+
+        // Sub setting array ( changing lower and upper bond
         else if(array[midPoint] < value ) lowerBond = midPoint + 1 // increasing lower bond to midpoint + 1
         else upperBond  = midPoint - 1 // decreasing upper bond to midPoint -1
     }
