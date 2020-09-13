1b0f7aa1d8b9c48d7c3be50cf121ffab9e548633
thisisxitij <xitij10@gmail.com>
2020-09-14 00:51:57 +0530

    fixed !

--- Sorting Algorithms/bubble_sort/BubbleSort.kt
+++ Sorting Algorithms/bubble_sort/BubbleSort.kt
@@ -1,12 +1,13 @@
 package bubble_sort
 
 fun main() {
-    val array = arrayOf(50, 20, 30, 1, 2, 5)
-    println("Unsorted Array: ${array.toList()}")
+    val arrayOne = arrayOf(64, 34, 25, 12, 22, 11, 90)
+    println("Unsorted Array One: ${arrayOne.toList()}")
 
     // sort array
-    bubbleSortWithWhile(array)
-    println("Sorted Array: ${array.toList()} ")
+    bubbleSortWithWhile(arrayOne)
+    println("With while loop !")
+    println("Sorted Array Two: ${arrayOne.toList()}")
 
 }
 
@@ -17,16 +18,15 @@ fun main() {
 fun bubbleSortWithWhile(array: Array<Int>) {
 
     val n = array.size -1 // last index of array
-
     var i = 0
     var swapped: Boolean // to check the array is already sorted
+
     while(i < n) {
         swapped = false
 
         // for traversing the array
         var j = 0
         while(j < n -i -1) { // because we know last value in array is already sorted after one iteration of outer while gets completed
-
 
             // swap the value at j  with value at j+1 if the value is greater at index j then the value at index j +1
             if (array[j] > array[j +1]) {
@@ -37,14 +37,10 @@ fun bubbleSortWithWhile(array: Array<Int>) {
                 // and the value is swapped means our array is not fully sorted yet
                 swapped = true
             }
-
             j++
         }
-
         if(!swapped) break // array is sorted
-
         i++
     }
 }
-
 
